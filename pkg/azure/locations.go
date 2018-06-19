6af707badb9cdb65eb0a6f3eec28951c363105fe
Zoltan Fuves <Zoltan.Fueves@t-systems.com>
2018-06-19 16:13:46 +0200

    update from FlorinPeter to test broker on the German Cloud(s)

--- pkg/azure/locations.go
+++ pkg/azure/locations.go
@@ -33,6 +33,8 @@ var locations = []string{
 	"westindia",
 	"westus",
 	"westus2",
+	"germanycentral", 
+	"germanynortheast", 
 }
 
 // IsValidLocation returns a bool indicating whether the provided location is a
